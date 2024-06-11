f6fcc46621c30b61629972d93f49df7acdffd538
Waldow <dwaldow@me.com>
2024-06-11 15:12:33 +0100

    undo breaking change

--- src/components/DatastreamSelect.tsx
+++ src/components/DatastreamSelect.tsx
@@ -46,18 +46,24 @@ export const DatastreamSelect = (
   }, [selectedDatastream]);
 
   useEffect(() => {
+    if (selectedDatastream) {
+      const { url } = JSON.parse(selectedDatastream) as OptionValue;
+      const urlWithParams = buildUrlWithParams(url, { '$count': true, '$top': 500, '$filter': `year(phenomenonTime) eq 2023` });
+      setObservationsUrl(urlWithParams);
+    }
+  }, [selectedDatastream]);
+
+  useEffect(() => {
     if (isStale && isPending) {
       setObservationsLoading(true);
     }
-  }, [isPending, isStale, setObservationsLoading]);
 
-  useEffect(() => {
-    if (!isPending && !isError && !isFetching) {
+    if (!isPending && !isError) {
       setObservationsCount(data['@iot.count'] || 0);
       setObservationsData(data.value);
       setObservationsLoading(false);
     }
-  }, [data, isPending, isFetching, isError, setObservationsData, setObservationsCount, setObservationsLoading]);
+  }, [data, isPending, isStale, isError, setObservationsData, setObservationsCount, setObservationsLoading]);
 
   return (
     <div className="selector-container">
